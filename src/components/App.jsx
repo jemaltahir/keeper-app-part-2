c3ab1fa0ec2fdc41491c43d01e5fbe556fcf1b45
Jemal Tahir <jemalmtahir@gmail.com>
2020-03-16 22:05:25 +0200

    addnote, deletenote implelmented
    note app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  function deleteNote(id) {
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
+  function addNote(newNote) {
+    setNotes(prevNotes => {
+      return [...prevNotes, newNote];
+    });
+  }
   return (
     <div>
       <Header />
-      <CreateArea />
-      <Note key={1} title="Note title" content="Note content" />
+      <CreateArea onAddNote={addNote} />
+      {notes.map((note, index) => {
+        return (
+          <Note
+            id={index}
+            key={index}
+            title={note.title}
+            content={note.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
+
       <Footer />
     </div>
   );
